11fa1f58dfd59c5f76fd336bcd4d88d6f36f055c
Sondre Matre <sondrema4@gmail.com>
2024-01-10 14:11:23 +0100

    lagt til githubcommands, koblet til mongodb

---
+++ app1/app/lib/mongodb.js
@@ -0,0 +1,27 @@
+require('dotenv').config({ path: '.env.local' });
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Bruk URI fra miljøvariabler
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
