1397d9433f12cfd69214c5104b990ee554e96cde
florsalvas <rolf.salvesen@gmail.com>
2024-04-23 19:20:29 +0200

    gjenopprettet home page til før fil innlegging

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -4,15 +4,7 @@ import TranslationsProvider from '../components/TranslationsProvider';
 import News from '../components/news';
 import ButtonOnPicture from "../components/buttonOnPicture";
 import initTranslations from '../i18n';
-import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-
-/*
-import { getClient } from "@/app/utils/db"; kan ikkje vær i klient sida
-
-const database = await getClient();
-const users = database.collection("users");
-*/
 
 const i18nNamespaces = ['Home', 'Common'];
 
@@ -21,58 +13,7 @@ export default function Home({ params:{locale}}) {
   const { t, resources } = initTranslations(locale, i18nNamespaces);
 
   const { data: session } = useSession();
-  const [file, setFile] = useState(null); // flytte denne opp og ut av function home
-  /*
-  const loggedUser = session.user?.email;
-  const adminCheck = "admin";
 
-  const curUser = users.findOne({ adminCheck })
-  console.log(curUser);
-  var curAdmin = null;
-
-  if (!curUser) {
-    console.log("Admin ikke funnet");
-    curAdmin = 0;
-  } else {
-    console.log("Admin er funnet")
-    curAdmin = 1;
-  }
-  */
-
-
-const handleFileChange = async (e) => {
-  console.log("handleFileChange called");
-  const selectedFile = e.target.files[0];
-  console.log(selectedFile);
-  setFile(selectedFile); // const file forblir null
-}
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  console.log("handleSubmit reached");
-  try {
-  
-    console.log(file);
-    const formData = new FormData();
-    formData.append('file', file);
-    console.log(formData);
-
-    const response = await fetch("../api/imgUpload", {
-      method: "POST", // POST for å sende data
-      body: formData,
-    });
-
-    if (response) {
-      const data = await response.json();
-      console.log(data);
-      console.log("response caught")
-    }
-  }
-  catch (e) {
-    console.log(e);
-    console.log("error caught")
-  }
-}
 
   return (
    
