2ab4227f250d6e10492ee8e1b749e93ec82cd572
Sondre Matre <sondrema4@gmail.com>
2024-04-28 12:57:33 +0200

    fikset artikkel i hjemmesiden

--- app/components/editingComponent.js
+++ app/components/editingComponent.js
@@ -7,8 +7,8 @@ const ArticleEditor = ({ initialContent, handleEditorChange }) => {
   return (
     <Editor
     apiKey='er9zpdxj4m98bmh0iw4l8b6eew8v0nvfml8qurvih265ag7b'
-    
     init={{
+      
       plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
       toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
       tinycomments_mode: 'embedded',
